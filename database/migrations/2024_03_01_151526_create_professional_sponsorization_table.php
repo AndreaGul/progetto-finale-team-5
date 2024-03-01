a3d8e2e54eab0ea501bb307e136dbaf00152d137
matteomedori <matteo.medori.97@gmail.com>
2024-03-01 16:26:05 +0100

    feat: pivot table professional sponsorization

---
+++ database/migrations/2024_03_01_151526_create_professional_sponsorization_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('professional_sponsorization', function (Blueprint $table) {
+            $table->unsignedBigInteger('professional_id');
+            $table->foreign('professional_id')->references('id')->on('professionals')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('sponsorization_id');
+            $table->foreign('sponsorization_id')->references('id')->on('sponsorizations')->cascadeOnDelete();
+
+            $table->primary(['professional_id', 'sponsorization_id']);
+            $table->dateTime('date_end_sponsorization');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('professional_sponsorization');
+    }
+};
