9763581af71d35eeb91495d2d1e531090bee1d20
matteomedori <matteo.medori.97@gmail.com>
2024-03-02 11:22:33 +0100

    feat: models relationships

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
+class Vote extends Model
 {
     use HasFactory;
 
     public function professional()
     {
         return $this->belongsTo(Professional::class);
+    }
+
+    public function lookupVote()
+    {
+        return $this->belongsTo(LookupVote::class);
     }
 }
