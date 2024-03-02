99cbec02605312860395b05d8650f214e8aa39b7
matteomedori <matteo.medori.97@gmail.com>
2024-03-02 11:00:20 +0100

    feat: table votes

---
+++ database/migrations/2024_03_02_095355_create_votes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('professional_id')->nullable();
+            $table->foreign('professional_id')->references('id')->on('professionals')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('lookup_id')->nullable();
+            $table->foreign('lookup_id')->references('id')->on('lookup_votes')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes');
+    }
+};
