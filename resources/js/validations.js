c3292eb00cb1235c8bdbdf71a627a8179c80ed74
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-03-04 15:39:26 +0100

    feat: validations

---
+++ resources/js/validations.js
@@ -0,0 +1,87 @@
+const required = ['name', 'surname', 'phone', 'address'];
+/*
+curriculum
+photo
+*/
+
+const elementsText = [];
+const elementsEmail = [];
+const elementsNumber = [];
+const elementsBigText = [];
+
+const alert = document.getElementById('error-fe');
+const errorText = document.getElementById('error-text');
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+
+elementsText.push(document.getElementById('name'));
+elementsText.push(document.getElementById('surname'));
+elementsNumber.push(document.getElementById('phone'));
+elementsBigText.push(document.getElementById('address'));
+elementsBigText.push(document.getElementById('performance'));
+
+submit.addEventListener('click', (event) => {
+    let authorize = true;
+    // Small Text
+    elementsText.forEach((element) => {
+        if(required.includes(element.id)){
+            if(element.value.length < 3 || element.value.length > 20){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else if(/[^a-zA-Z]/.test(element.value)){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else{
+                element.classList.remove('border', 'border-danger');
+            }
+        }
+    });
+
+    // Email
+    elementsEmail.forEach((element) => {
+        if(required.includes(element.id)){
+            if(!element.value.includes('@') && !element.value.includes('.')){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else{
+                element.classList.remove('border', 'border-danger');
+            }
+        }
+    });
+
+    // Number
+    elementsNumber.forEach((element) => {
+        if(required.includes(element.id)){
+            if(isNaN(element.value)){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else if((element.id === 'phone' && element.value.length > 20) || (element.id === 'phone' && element.value.length < 10)){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else if(element.value.length > 255 || element.value.length < 1){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else{
+                element.classList.remove('border', 'border-danger');
+            }
+        }
+    });
+
+    // Text
+    elementsBigText.forEach((element) => {
+        if(required.includes(element.id)){
+            if(element.id === 'address' && element.value.length > 255  || element.value.length < 5){
+                element.classList.add('border', 'border-danger');
+                authorize = false;
+            }else{
+                element.classList.remove('border', 'border-danger');
+            }
+        }
+    });
+    
+    if(authorize !== true){
+        event.preventDefault();
+        alert.classList.remove('d-none');
+        errorText.innerText = 'Campi non validi';
+    }
+});
