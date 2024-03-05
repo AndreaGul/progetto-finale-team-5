6598908b692285ae34d3cf5801cb36432e27007d
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-03-05 17:06:09 +0100

    feat: api professional

--- app/Http/Controllers/Api/ProfessionalController.php
+++ app/Http/Controllers/Api/ProfessionalController.php
@@ -4,33 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Professional;
+use App\Models\Specialization;
 use Illuminate\Http\Request;
 
 class ProfessionalController extends Controller
 {
-    public function index()
+    public function index($param)
     {
-        request()->key = 110;
-        request()->validate([
-            'key' => ['max:5']
-        ]);
+        if(!empty($param)){
+            // validazione
+            $values = explode(',', $param);
+            $ids = [];
+            $specializations = Specialization::pluck('id')->toArray();
+            foreach($values as $value){
+                if(in_array($value, $specializations))$ids[] = $value;
+            }
 
-        dd(request()->key);
-        $values = explode(',', request()->key);
-
-        foreach ($values as $value) {
-            request()->validate([
-                $value => ['!exists:specializations,id']
-            ]);
+            if(count($ids) > 0){
+                // se ci sono id validi | mostra solo l'ultima sponsorizzazione e solo se la data di fine è maggiore
+                $current_time = now();// data e ora
+                $professionals = Professional::whereHas('specializations', function ($query) use ($ids){
+                    $query->whereIn('id', $ids);
+                })->with(['sponsorizations' => function ($query) use($current_time){
+                    $query->withPivot('professional_id', 'sponsorization_id', 'date_end_sponsorization')->where('date_end_sponsorization', '>', $current_time)->orderBy('date_end_sponsorization', 'desc')->limit(1);
+                }])->paginate(10);
+                return response()->json([
+                    'status' => 'success',
+                    'data' => $professionals
+                ]);
+            }else{
+                // se gli di non sono validi
+                return response()->json([
+                    'status' => 'error',
+                    'data' => 'invalid ids'
+                ]);
+            }
         }
-
-        $professionals = Professional::whereHas('specializations', function ($query) {
-            $query->whereIn('id', [1, 2]);
-        })->get();
-        // dd($professionals);
-        return response()->json([
-            'status' => 'success',
-            'data' => $professionals
-        ]);
     }
 }
