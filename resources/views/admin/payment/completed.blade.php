4b762cdce10855165028c7385c95cdf956934208
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-03-12 12:34:18 +0100

    feat: pagina pagamento completato

--- resources/views/admin/payment/completed.blade.php
+++ resources/views/admin/payment/completed.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.admin')
 
 @section('content')
-    <p>Pagamento effettuato con successo!</p>
-    <a class="btn btn-primary" href="{{ route('admin.dashboard')}}" role="button">Ritorna alla pagina principale</a>
+    <h1 class="mt-5">Pagamento effettuato con successo!</h1>
+    <p>Stai per essere reindirizzato alla pagina principale</p>
+    <a class="" href="{{ route('admin.dashboard')}}" role="button">Ritorna alla pagina principale</a>
+    <script>
+        setTimeout(function(){window.location="http://127.0.0.1:8000/admin/info"}, 3000);
+    </script> 
 @endsection
