42c3ee9b6449df6777dac0e34c71e1f93f07c986
AndreaGul <andreagulino99@gmail.com>
2024-03-14 10:45:43 +0100

    feat:mostra in pagina numero di recensioni e di messaggi

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <h1 class="text-primary mt-3 text-center">Messaggi</h1>
     @if ($messages->count())
+        <h3>Numero di messaggi: {{$messages->count()}} </h3>
         <ul class="list-unstyled">
             @foreach ($messages as $message)
                 <li class="mb-3">
